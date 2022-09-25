4a14c92050bdc1599c7e0f938a865923fd4f075d
ProgramingIsTheFuture <franciscosantosfcp@hotmail.com>
2022-09-25 21:01:24 +0100

    Function types with some tests

--- test/lextest.ml
+++ test/lextest.ml
@@ -9,7 +9,8 @@ let basic_variables () =
             :: Var "a" :: []  in
   assert (exp = t);;
 
-let complex_types () =
+let complex_inference () =
+  (* TODO *)
   let code = "let sum (a: int): int -> int = a;" in
   let t = Lex.parse ~code () in
 
@@ -18,7 +19,27 @@ let complex_types () =
             :: [] in
   assert (exp = t);;
 
+let complex_types () =
+  (* Is returning a string but expected a int *)
+  let code = "let sum (a: string) (b: int): string -> int -> int = a;" in
+  try
+    Lex.parse ~code () |> ignore;
+    assert false
+  with
+  | Lex.Error.InvalidType _ ->
+    assert true;;
+
+let invalid_types () =
+  (* First input is a string and a int? = Type error *)
+  let code = "let sum (a: string) (b: int): int -> int -> int = a;" in
+  try
+    Lex.parse ~code () |> ignore;
+    assert false
+  with
+  | Lex.Error.InvalidType _ ->
+    assert true;;
+
 let () =
-  [basic_variables; complex_types]
+  [basic_variables; complex_inference; complex_types; invalid_types;]
   |> List.map (fun f -> f ())
   |> ignore;;
