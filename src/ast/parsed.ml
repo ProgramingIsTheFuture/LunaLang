f569cce514dd9595c6f9a6e44cda4bc24b67836e
ProgramingIsTheFuture <franciscosantosfcp@hotmail.com>
2023-06-16 18:19:43 +0100

    Improving pretty print

--- src/ast/parsed.ml
+++ src/ast/parsed.ml
@@ -16,22 +16,31 @@ type expr =
 
 and t = { expr : expr; pos : pos }
 
-let rec pp_t = function
+let rec pp_t_ident ast =
+  match ast with
   | { expr = Const v; _ } -> pp_values v
   | { expr = Var s; _ } -> Format.sprintf "%s" s
-  | { expr = LetIn (s, t, e1, Some e2); _ } -> (
-      match t with
-      | None -> Format.sprintf "let %s = %s in %s" s (pp_t e1) (pp_t e2)
-      | Some t ->
-          Format.sprintf "let %s : %s = %s in %s" s t (pp_t e1) (pp_t e2))
-  | { expr = LetIn (s, t, e1, None); _ } -> (
-      match t with
-      | None -> Format.sprintf "let %s = %s\n" s (pp_t e1)
-      | Some t -> Format.sprintf "let %s : %s = %s\n" s t (pp_t e1))
-  | { expr = Fun (s, t, e1); _ } -> (
-      match t with
-      | None -> Format.sprintf "fun %s -> %s\n" s (pp_t e1)
-      | Some t -> Format.sprintf "fun (%s : %s) -> %s\n" s t (pp_t e1))
-  | { expr = App (e1, e2); _ } -> Format.sprintf "(%s %s)" (pp_t e1) (pp_t e2)
+  | { expr = LetIn (s, t, e1, Some e2); _ } ->
+      let t =
+        match t with None -> " " | Some t -> Format.sprintf " : %s " t
+      in
+      Format.sprintf "let %s%s= %s in %s" s t (pp_t_ident e1) (pp_t_ident e2)
+  | { expr = LetIn (s, t, e1, None); _ } ->
+      let t =
+        match t with None -> " " | Some t -> Format.sprintf " : %s " t
+      in
+      Format.sprintf "let %s%s= %s;\n" s t (pp_t_ident e1)
+  | { expr = Fun (s, t, e1); _ } ->
+      let t =
+        match t with
+        | None -> Format.sprintf "%s" s
+        | Some t -> Format.sprintf "(%s : %s)" s t
+      in
+      Format.sprintf "fun %s -> %s" t (pp_t_ident e1)
+  | { expr = App (e1, e2); _ } ->
+      Format.sprintf "(%s %s)" (pp_t_ident e1) (pp_t_ident e2)
   | { expr = IfThen (b, e1, e2); _ } ->
-      Format.sprintf "if %s then %s else %s" (pp_t b) (pp_t e1) (pp_t e2)
+      Format.sprintf "if %s then %s else %s" (pp_t_ident b) (pp_t_ident e1)
+        (pp_t_ident e2)
+
+let pp_t t = pp_t_ident t |> Format.sprintf "%s"
--- src/ast/typed.ml
+++ src/ast/typed.ml
@@ -1,6 +1,15 @@
 open Common
 
-type typ = Int | String | Product of typ * typ | Tarrow of typ * typ
+type typ =
+  | Tbool
+  | Tint
+  | Tstr
+  | Tproduct of typ * typ
+  | Tarrow of typ * typ
+  | Tvar of tvar
+  | Tunit
+
+and tvar = { id : int; mutable def : typ option }
 
 type expr =
   | Const of values
@@ -15,3 +24,30 @@ type expr =
   | IfThen of t * t * t
 
 and t = { expr : expr; pos : pos; typ : typ }
+
+let rec pp_typ = function
+  | Tbool -> "bool"
+  | Tint -> "int"
+  | Tstr -> "str"
+  | Tproduct (t1, t2) -> Format.sprintf "(%s * %s)" (pp_typ t1) (pp_typ t2)
+  | Tarrow (t1, t2) -> Format.sprintf "%s -> %s" (pp_typ t1) (pp_typ t2)
+  | Tvar { def = Some t; _ } -> pp_typ t
+  | Tvar { def = None; _ } -> "'a"
+  | Tunit -> "()"
+
+let rec pp_t = function
+  | { expr = Const v; typ; _ } ->
+      Format.sprintf "([typ %s]%s)" (pp_typ typ) (pp_values v)
+  | { expr = Var s; typ; _ } -> Format.sprintf "([typ %s]%s)" (pp_typ typ) s
+  | { expr = LetIn (s, e1, Some e2); typ; _ } ->
+      Format.sprintf "([typ %s]let %s = %s in %s)" (pp_typ typ) s (pp_t e1)
+        (pp_t e2)
+  | { expr = LetIn (s, e1, None); typ; _ } ->
+      Format.sprintf "[typ %s]let %s = %s;\n" (pp_typ typ) s (pp_t e1)
+  | { expr = Fun (s, e1); typ; _ } ->
+      Format.sprintf "[typ %s](fun (%s) -> %s)\n" (pp_typ typ) s (pp_t e1)
+  | { expr = App (e1, e2); typ; _ } ->
+      Format.sprintf "[typ %s](%s %s)" (pp_typ typ) (pp_t e1) (pp_t e2)
+  | { expr = IfThen (b, e1, e2); typ; _ } ->
+      Format.sprintf "[typ %s](if %s then %s else %s)" (pp_typ typ) (pp_t b)
+        (pp_t e1) (pp_t e2)
