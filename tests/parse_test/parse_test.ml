172d5461f3b74dc2462de9edaaa5dd08146f057e
ProgramingIsTheFuture <franciscosantosfcp@hotmail.com>
2023-06-16 18:20:32 +0100

    Testing more the parser and type checker

--- tests/parse_test/parse_test.ml
+++ tests/parse_test/parse_test.ml
@@ -1,22 +1,43 @@
 let%expect_test "Simple let" =
-  Parser.of_string "let a = 10"
+  Parser.of_string "let a = 10;"
   |> Parser.parse |> List.hd |> Ast.Parsing.pp_t |> print_string |> flush_all;
-  [%expect {| let a = 10 |}]
+  [%expect {|
+    let a = 10; |}]
 
 let%expect_test "Simple fun" =
-  Parser.of_string "let a = fun i -> i"
+  Parser.of_string "let a = fun i -> i;"
   |> Parser.parse |> List.hd |> Ast.Parsing.pp_t |> print_string |> flush_all;
-  [%expect {| let a = fun i -> i |}]
+  [%expect {|
+    let a = fun i -> i; |}]
 
 let%expect_test "Syntatic Sugar" =
   Parser.of_string "let a i = i"
   |> Parser.parse |> List.hd |> Ast.Parsing.pp_t |> print_string |> flush_all;
   [%expect {|
-    let a = fun i -> i |}]
+    let a = fun i -> i; |}]
+
+let%expect_test "Let unit expr" =
+  Parser.of_string "let a = 10; a"
+  |> Parser.parse
+  |> List.iter (fun a -> Ast.Parsing.pp_t a |> print_string |> flush_all);
+  [%expect {|
+    let a = 10;
+    a |}]
+
+let%expect_test "Variable" =
+  Parser.of_string "a" |> Parser.parse
+  |> List.iter (fun a -> Ast.Parsing.pp_t a |> print_string |> flush_all);
+  [%expect {|
+    a |}]
+
+let%expect_test "Let in" =
+  Parser.of_string "let a = 10 in a"
+  |> Parser.parse
+  |> List.iter (fun a -> Ast.Parsing.pp_t a |> print_string |> flush_all);
+  [%expect {| let a = 10 in a |}]
 
 let%expect_test "apply" =
   Parser.of_string "let f a = a in f true"
   |> Parser.parse |> List.hd |> Ast.Parsing.pp_t |> print_string |> flush_all;
   [%expect {|
-    let f = fun a -> a
-     in (f true) |}]
+    let f = fun a -> a in (f true) |}]
---
+++ tests/type_checker_test/type_checker_test.ml
@@ -0,0 +1,38 @@
+let%test "consts" =
+  Parser.of_string "10" |> Parser.parse
+  |> Type_checker.type_ast ~env:Type_checker.Env.empty
+  |> List.map (fun a ->
+         let open Ast.Typed in
+         a.typ = Ast.Typed.Tint)
+  |> List.fold_left (fun a b -> a && b) true
+
+let%test "if_expr" =
+  Parser.of_string "if true then 10 else 20"
+  |> Parser.parse
+  |> Type_checker.type_ast ~env:Type_checker.Env.empty
+  |> List.map (fun a ->
+         let open Ast.Typed in
+         a.typ = Ast.Typed.Tint)
+  |> List.fold_left (fun a b -> a && b) true
+
+let%test "Tvar" =
+  Parser.of_string "let a = 10 in a"
+  |> Parser.parse
+  |> Type_checker.type_ast ~env:Type_checker.Env.empty
+  |> List.map (fun a ->
+         let open Ast.Typed in
+         a.typ = Tint)
+  |> List.fold_left (fun a b -> a && b) true
+
+let%expect_test "let" =
+  Parser.of_string "let f a = a; let b = f 10; let c = f false;"
+  |> Parser.parse
+  (* |> List.iter (fun a -> Ast.Parsing.pp_t a |> print_string |> flush_all); *)
+  |> Type_checker.type_ast ~env:Type_checker.Env.empty
+  |> List.iter (fun a -> Ast.Typed.pp_t a |> print_string |> flush_all);
+  [%expect
+    {|
+    [typ ()]let f = [typ 'a -> 'a](fun (a) -> ([typ 'a]a))
+    ;
+    [typ ()]let b = [typ int](([typ int -> int]f) ([typ int]10));
+    [typ ()]let c = [typ bool](([typ bool -> bool]f) ([typ bool]false)); |}]
